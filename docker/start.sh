6ac56b22cdf009e85b00a16764f8e0f7c77c645a
Absa Fall <absa.fall@uadb.edu.sn>
2026-03-29 15:55:07 +0000

    fix cache path et app key

--- docker/start.sh
+++ docker/start.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
+mkdir -p /tmp/views
 php artisan key:generate --force
 php artisan migrate --force
 php artisan config:clear
-php artisan cache:clear
 php artisan view:clear
 php artisan route:clear
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
 apache2-foreground
